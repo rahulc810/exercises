4e8443dadc97a430f1aadb54c0f2395aa0432c5a
rahulc810 <rahulchauhan.83@gmail.com>
2018-03-19 22:31:07 +0530

    objstore done

--- easy/object_store.go
+++ easy/object_store.go
@@ -1,68 +1,70 @@
 package easy
 
-import(
+import (
 	"fmt"
-	"time"
 	"hash/fnv"
 	"strconv"
 	"sync"
 	"sync/atomic"
+	"time"
+
 	"github.com/rahulc810/exercises/utils"
 )
 
-type data struct{
-	text []byte
-	refs uint32
+type data struct {
+	text    []byte
+	refs    uint32
 	version int64
 }
 
-type event struct{
+type event struct {
 	eKey string
-	op	uint8
+	op   uint8
 }
 
-//id -> k1 
-var primary = make(map[uint64]string,1000)
+//id -> k1
+var primary = make(map[uint64]string, 1000)
+
 //k1->*text
-var secondary = make(map[string]*data,1000)
+var secondary = make(map[string]*data, 1000)
 var q = utils.NewObjQueue(10)
 
-var hashMap = make(map[uint64]*data,1000)
+var hashMap = make(map[uint64]*data, 1000)
 
 var pMutex = sync.RWMutex{}
 var sMutex = sync.RWMutex{}
 var iMutex = sync.Mutex{}
 var count uint64 = 0
 
-func Start(){
-	//go processQ()
-	go func(){
-		for{
+func Start() {
+	go processQ()
+	go func() {
+		for {
 			itrsec()
 			fmt.Println()
-			time.Sleep(5)
+			//time.Sleep(5 * time.Millisecond)
 		}
-	}() 
+	}()
 }
 
-//gaurentee uniqueness 
-func generateID() uint64{
+//gaurentee uniqueness
+func generateID() uint64 {
 	iMutex.Lock()
 	defer iMutex.Unlock()
-	return uint64(atomic.AddUint64(&count,1))
+	return uint64(atomic.AddUint64(&count, 1))
 }
 
-func getInternalID(pID uint64) string{
-	return strconv.FormatUint(pID,10) +  "sec"
+func getInternalID(pID uint64) string {
+	return strconv.FormatUint(pID, 10) + "sec"
 }
 
-func hash(text []byte)uint64{
-	h:=fnv.New64a()
+func hash(text []byte) uint64 {
+	h := fnv.New64a()
 	h.Write(text)
 	return h.Sum64()
 }
 
-func Put(text []byte) uint64{
+func Put(text []byte) uint64 {
 	id := generateID()
 	iid := getInternalID(id)
 
@@ -70,104 +72,102 @@ func Put(text []byte) uint64{
 	sMutex.Lock()
 	defer pMutex.Unlock()
 	defer sMutex.Unlock()
-	
-	primary[id] = iid
-	secondary[iid] = &data{text,1,time.Now().Unix()}
 
-	q.Enqueue(event{iid,1})
+	primary[id] = iid
+	secondary[iid] = &data{text, 1, time.Now().Unix()}
+
+	q.Enqueue(event{iid, 1})
 
 	return id
 }
 
-func Get(id uint64)([]byte,error){
+func Get(id uint64) ([]byte, error) {
 	pMutex.RLock()
 	sMutex.RLock()
 	defer pMutex.RUnlock()
 	defer sMutex.RUnlock()
 
-	iid,ok := primary[id]
-	if !ok{
-		return nil, fmt.Errorf("Key not found: %d",id)
+	iid, ok := primary[id]
+	if !ok {
+		return nil, fmt.Errorf("Key not found: %d", id)
 	}
-	ret,ok := secondary[iid]
-	if !ok{
-		return nil, fmt.Errorf("[Internal] Key not found: %s",iid)
+	ret, ok := secondary[iid]
+	if !ok {
+		return nil, fmt.Errorf("[Internal] Key not found: %s", iid)
 	}
-	return ret.text,nil 
+	return ret.text, nil
 }
 
-
-func Del(id uint64)error{
+func Del(id uint64) error {
 	pMutex.Lock()
 	sMutex.Lock()
 	defer pMutex.Unlock()
 	defer sMutex.Unlock()
 
-	iid,ok := primary[id]
-	if !ok{
-		return fmt.Errorf("Key not found: %d",id)
+	iid, ok := primary[id]
+	if !ok {
+		return fmt.Errorf("Key not found: %d", id)
 	}
-	delete(primary,id)
-	q.Enqueue(event{iid,2})
+	delete(primary, id)
+	q.Enqueue(event{iid, 2})
 	return nil
 }
 
-
-func processQ(){
-	for{
-		eve,err := q.Dequeue()
-		if err == nil && eve !=nil{
+func processQ() {
+	for {
+		eve, err := q.Dequeue()
+		if err == nil && eve != nil {
 			e := eve.(event)
-			switch e.op{
-			case 1:  //put
-				possibleDup := secondary[e.eKey] 
+			switch e.op {
+			case 1: //put
+				possibleDup := secondary[e.eKey]
 				h := hash(possibleDup.text)
-				duplicate,commonDataValue:=putInMap(h,possibleDup)
-				if duplicate{
-					secondary[e.eKey] = commonDataValue 
-				}	
-			case 2:	//del
-				dataValue := secondary[e.eKey] 
+				duplicate, commonDataValue := putInMap(h, possibleDup)
+				if duplicate {
+					secondary[e.eKey] = commonDataValue
+				}
+			case 2: //del
+				dataValue := secondary[e.eKey]
 				h := hash(dataValue.text)
-				removeFromMap(e,h)
+				removeFromMap(e, h)
 			}
 			fmt.Printf("HASHMAP => %v\n", hashMap)
 		}
 	}
 }
 
-
-func getFromMap(key uint64) (*data,bool){
-	ret,ok := hashMap[key] 
-	return ret,ok
+func getFromMap(key uint64) (*data, bool) {
+	ret, ok := hashMap[key]
+	return ret, ok
 }
 
-func putInMap(key uint64, d *data) (bool, *data){
-	ret,ok := hashMap[key] 
-	if !ok{
-		hashMap[key]= d
-		return false,nil
-	}else{
+func putInMap(key uint64, d *data) (bool, *data) {
+	ret, ok := hashMap[key]
+	if !ok {
+		hashMap[key] = d
+		return false, nil
+	} else {
 		ret.refs++
-		return true,ret
+		return true, ret
 	}
 }
 
-func removeFromMap(e event, key uint64){
-	ret,ok := hashMap[key] 
-	if !ok{
-	}else{
+func removeFromMap(e event, key uint64) {
+	ret, ok := hashMap[key]
+	if !ok {
+	} else {
+		fmt.Printf("Removing from secondary => %v <%v>\n", ret, secondary[e.eKey])
 		ret.refs--
-		if ret.refs < 2{
-			delete(secondary, e.eKey)
-			delete(hashMap,key)
+		sMutex.Lock()
+		delete(secondary, e.eKey)
+		sMutex.Unlock()
+		if ret.refs < 1 {
+			delete(hashMap, key)
 		}
-	}	
+	}
 }
 
-
-
-func itrsec(){
+func itrsec() {
 	sMutex.RLock()
 	defer sMutex.RUnlock()
 
--- main.go
+++ main.go
@@ -2,40 +2,50 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"strconv"
 	"strings"
 	//"github.com/rahulc810/exercises/utils"
+
 	"github.com/rahulc810/exercises/easy"
 )
 
 func main() {
+	//defer profile.Start(profile.MemProfile).Stop()
 	easy.Start()
-	easy.Put([]byte("ABC"))
-	easy.Put([]byte("ABC"))
-	easy.Put([]byte("ABC"))
-	easy.Put([]byte("ABC"))
+	id1 := easy.Put([]byte("ABC"))
+	id2 := easy.Put([]byte("ABC"))
+	id3 := easy.Put([]byte("ABC"))
+	id4 := easy.Put([]byte("ABC"))
 
-	for{}
-	
-//easy.Listen() -- hike server listen
-//clockMain()
-//easy.BaseMain()
-//easy.AffixCipherMain()
-//easy.RabbitMain()
+	for i := 0; i < 1000000; i++ {
+	}
+	runtime.GC()
+	easy.Del(id1)
+	easy.Del(id2)
+	easy.Del(id3)
+	easy.Del(id4)
+	for {
+	}
+	//easy.Listen() -- hike server listen
+	//clockMain()
+	//easy.BaseMain()
+	//easy.AffixCipherMain()
+	//easy.RabbitMain()
 }
 
-func clockMain(){
-inputs := []string{"00:00", "03:45", "07:01", "13:19"}
-for idx, val := range inputs {
-	s := strings.Split(val, ":")
-	hh, _ := strconv.Atoi(s[0])
-	mm, _ := strconv.Atoi(s[1])
+func clockMain() {
+	inputs := []string{"00:00", "03:45", "07:01", "13:19"}
+	for idx, val := range inputs {
+		s := strings.Split(val, ":")
+		hh, _ := strconv.Atoi(s[0])
+		mm, _ := strconv.Atoi(s[1])
 
-	fmt.Printf("[%d] Parsed input<= %d:%d\n", idx, hh, mm)
+		fmt.Printf("[%d] Parsed input<= %d:%d\n", idx, hh, mm)
 
-	hours, phase := easy.ConvertHours(hh)
+		hours, phase := easy.ConvertHours(hh)
 
-	fmt.Printf("[%d] Output=> %s %s %s \n", idx, easy.TranslateToWords(hours, false), easy.TranslateToWords(mm, true), phase)
+		fmt.Printf("[%d] Output=> %s %s %s \n", idx, easy.TranslateToWords(hours, false), easy.TranslateToWords(mm, true), phase)
 
-}
+	}
 }
