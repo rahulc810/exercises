c81abcd788a10664f7015e301ba168314eabe812
Rahul Chauhan <rahchauh@adobe.com>
2018-04-03 16:47:59 +0530

    pretty print

--- utils/priority_queue.go
+++ utils/priority_queue.go
@@ -36,6 +36,66 @@ func preToIn(pre []interface{}, in []interface{}) {
 		//
 	}
 }
+func (h *HeapArr) printPretty() {
+	filler := " "
+	l := len(h.arr)
+	lvls := int(math.Ceil(math.Log2(float64(l))))
+	nodesOnLastLvl := 1 << (uint64(lvls - 1))
+	tPossibleNodes := (1 << (uint32(lvls))) - 1
+	fmt.Println("Total levels: ", lvls)
+	fmt.Println("Nodes in last level: ", nodesOnLastLvl)
+	fmt.Println("Total possible nodes in the tree: ", tPossibleNodes)
+	tCols := (nodesOnLastLvl-1)*3 + nodesOnLastLvl //spacesBetweenNodes for last lvl is alway 3
+	tRows := tPossibleNodes
+
+	idx := 0
+	numberOfBlanksRows := 0
+	fmt.Printf("Gibber %v %v\n", tRows, numberOfBlanksRows)
+	r := 1
+	for lv := 1; lv <= lvls; lv++ {
+		if r == 1 {
+			p(filler, (tCols-1)/2)
+			p(h.arr[idx], 1)
+			idx++
+			p(filler, (tCols-1)/2)
+			p("\n", 1)
+			r++
+			continue
+		}
+		numberOfNodes := 1 << (uint32(lv) - 1)
+		spacesBetweenNodes := (1 << (uint32(lvls - lv + 2))) - 1
+		spacesOnEnds := tCols - ((numberOfNodes - 1) * spacesBetweenNodes) - numberOfNodes
+		for ro := 1; ro <= (1<<uint32(lvls-lv+1))-1; ro++ {
+			for nodes := 1; nodes <= lv-1; nodes++ {
+				p(filler, spacesOnEnds/2)
+				p(filler, 1)
+				p(filler, (spacesBetweenNodes/2)-ro)
+				p("/", 1)
+				p(filler, 2*ro-1)
+				p("\\", 1)
+				p(filler, (spacesBetweenNodes/2)-ro)
+				p(filler, spacesBetweenNodes)
+			}
+			p(filler, spacesOnEnds/2)
+			p("\n", 1)
+		}
+
+		p(filler, spacesOnEnds/2)
+		for s := 0; s < numberOfNodes; s++ {
+			if idx >= l {
+				p("x", 1)
+			} else {
+				p(h.arr[idx], 1)
+			}
+			idx++
+			if s < numberOfNodes-1 {
+				p(filler, spacesBetweenNodes)
+			}
+		}
+		p(filler, spacesOnEnds/2)
+		p("\n", 1)
+	}
+}
 
 func (h *HeapArr) print() {
 	filler := "*"
@@ -81,6 +141,12 @@ func (h *HeapArr) print() {
 
 }
 
+func p(filler interface{}, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Printf("%v", filler)
+	}
+}
+
 func NewHeap() Heap {
 	arr := make([]interface{}, 0)
 	return &HeapArr{arr, 0}
@@ -89,6 +155,6 @@ func NewHeap() Heap {
 func Exec() {
 	sl := []interface{}{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
 	h := &HeapArr{sl, 0}
-	h.print()
+	h.printPretty()
 
 }
